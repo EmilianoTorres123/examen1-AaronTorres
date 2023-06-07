bd39ca78ed1284854c012d013eab3dd7bb0a758f
emiliano <emiliontn@hotmail.com>
2023-06-07 12:16:48 -0600

    subir examengabo

---
+++ src/main/java/org/uv/examen1emilianoTorres/Examen1emilianoTorresApplication.java
@@ -0,0 +1,13 @@
+package org.uv.examen1emilianoTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen1emilianoTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen1emilianoTorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/examen1emilianoTorres/Examen1emilianoTorresApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.examen1emilianoTorres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen1emilianoTorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
